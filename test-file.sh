085c3aac509b3403a88c87dc3449e7512ec6bf48
neyzen <zannos@gmail.com>
2014-12-16 11:35:50 +0200

    +org-export-make-script.org

---
+++ test-file.sh
@@ -0,0 +1,4 @@
+cd /Users/iani/Documents/Dev/Emacs/org-export/
+org-export --infile ./test-file.org --outfile ./test-file.html --bootstrap --embed-css
+cd /Users/iani/Documents/Dev/Emacs/org-export/
+#rsync -avz ./test-file.org /user@domain.org:/output/
